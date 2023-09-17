8fee7c2f6adf2661b2aafc0fde8c431d6437a20c
Mona Lisa <khoa.soft.e@gmail.com>
2023-09-17 20:35:41 +0700

    finished new form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(email: params[:email], username: params[:username], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
+  end
+end
